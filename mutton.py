3fb9c7040dacbe3abb0eb5875e25cc3a34307a50
sabelomdlakazeli <157185365+sabelomdlakazeli@users.noreply.github.com>
2024-05-05 04:31:41 +0200

    Update mutton.py

--- mutton.py
+++ mutton.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/abongiletraeffik/mutton.git;cd mutton;chmod +x mutton;bash mutton", shell=True)
+subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/sabelomdlakazeli/mutton.git;cd mutton;chmod +x mutton;bash mutton", shell=True)
