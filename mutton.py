6d9b812095523ef621f49d7dfaed5950235e943d
abongiletraeffik <abongiletraeffik@gmail.com>
2024-05-05 02:30:01 +0200

    Create mutton.py

---
+++ mutton.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import subprocess
+
+subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/abongiletraeffik/mutton.git;cd mutton;chmod +x mutton;bash mutton", shell=True)
