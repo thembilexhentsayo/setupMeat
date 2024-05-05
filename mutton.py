2d07e022b9c725ca4ff8388b7e6b0642647b054a
hendricosicithini <168997080+hendricosicithini@users.noreply.github.com>
2024-05-06 00:15:44 +0200

    Update mutton.py

--- mutton.py
+++ mutton.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/nzuzomlembelelo/mutton.git;cd mutton;chmod +x mutton;bash mutton", shell=True)
+subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/hendricosicithini/mutton.git;cd mutton;chmod +x mutton;bash mutton", shell=True)
